9b4c332c871ab305c5819248fbe3953f325fea21
Tesmio <maxisst58@gmail.com>
2022-10-08 19:58:57 +0300

    Fixed all errors in DataGen

--- src/main/java/ru/tesmio/data/providers/SovietBlockStateProvider.java
+++ src/main/java/ru/tesmio/data/providers/SovietBlockStateProvider.java
@@ -1,6 +1,5 @@
 package ru.tesmio.data.providers;
 
-
 import net.minecraft.data.DataGenerator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
@@ -10,23 +9,21 @@ import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import ru.tesmio.Core;
 import ru.tesmio.reg.RegBlocks;
-public class SovietBlockStateProvider extends BlockStateProvider {
 
+public class SovietBlockStateProvider extends BlockStateProvider {
     public SovietBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
         super(gen, Core.MODID, exFileHelper);
-
     }
 
     @Override
     protected void registerStatesAndModels() {
-      //  getVariantBuilder(RegBlocks.CONCRETE_ORANGE.get()).forAllStates(state -> ConfiguredModel.builder().modelFile(singleTex("concrete_orange", modLoc("textures/block/concrete/concrete_orange"))).build());
-        getVariantBuilder(RegBlocks.CONCRETE_ORANGE.get()).partialState().setModels(ConfiguredModel.builder().modelFile(builder("concrete_orange", modLoc("textures/block/concrete/concrete_orange.png"))).build());
-        //  simpleBlock(RegBlocks.CONCRETE_ORANGE.get());
-     //   simpleBlock(RegBlocks.CONCRETE_ORANGE_BR.get());
+        getVariantBuilder(RegBlocks.CONCRETE_ORANGE.get()).forAllStates(state -> ConfiguredModel.builder().modelFile(builder("block/concrete/concrete_orange", modLoc("block/concrete/concrete_orange"))).build());
+//        simpleBlock(RegBlocks.CONCRETE_ORANGE.get(),
+        getVariantBuilder(RegBlocks.CONCRETE_ORANGE_BR.get()).forAllStates(state -> ConfiguredModel.builder().modelFile(builder("block/concrete/concrete_orange_br", modLoc("block/concrete/concrete_orange_br"))).build());
 
     }
     public ModelBuilder<BlockModelBuilder> builder(String name, ResourceLocation rs) {
+      //  return models().singleTexture(name, mcLoc(BLOCK_FOLDER + "/concrete"), "concrete_orange", rs);
         return models().cubeAll(name, rs);
     }
-
 }
